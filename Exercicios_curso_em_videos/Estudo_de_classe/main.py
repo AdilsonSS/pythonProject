b798e8b6c18deb825ce71ce50141d57f040a91e9
Adilson Santana <adilson.tisuporte@hotmail.com>
2023-09-18 23:23:02 -0300

    Atualizações

--- Exercicios_curso_em_videos/Estudo_de_classe/main.py
+++ Exercicios_curso_em_videos/Estudo_de_classe/main.py
@@ -1,16 +1,17 @@
 from pessoa_fisica import PessoaFisica
 from pessoa_juridica import PessoaJuridica
 
-a = PessoaFisica('111.222.333-44', 'Alfredo', 22)
+a = PessoaFisica('951.369.471-28', 'Alfredo', 22)
 
 print('\nPessoa Física')
 print(f'Nome: {a.getNome()}')
 print(f'CPF: {a.getCPF()}')
 print(f'Idade: {a.getIdade()}')
 
-b = PessoaJuridica(CNPJ='64.614.527/0001-99', nome='Empresa X', idade=40)
+b = PessoaJuridica(CNPJ='83.926.467/6761-04', nome='Empresa X', idade=40)
 
 print('\nPessoa Jurídica:')
 print(f'Nome: {b.getNome()}')
 print(f'CNPJ: {b.getCNPJ()}')
 print(f'Idade: {b.getIdade()}\n')
+
--- Exercicios_curso_em_videos/funcoes.py
+++ Exercicios_curso_em_videos/funcoes.py
@@ -16,4 +16,5 @@ def calcular_pagamento(qtd_horas, valor_hora):
     salario = 40*taxa+(h_excd*(1.5*taxa))
   return salario
 
-
+a = calcular_pagamento(200,10)
+print(a)
